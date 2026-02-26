2229fee12d9aeada05c40b2f3bafddca0a319069
x1u49jd <jakub.dzido00@gmail.com>
2026-02-26 02:35:47 +0000

    Refactored Game class to initialise JFrame and JPanel in the constructor and drew player rectangle

--- Game.java
+++ Game.java
@@ -1,9 +1,22 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import java.awt.Graphics;
+
+import java.awt.Color;
+
 
 public class Game {
-    public static void main(String[] args) {
-        JFrame window = new JFrame();
-        
+    JFrame window;
+    JPanel panel;
+
+    int playerX = 400;
+    int playerY = 400;
+    int playerSpeed = 5;
+
+    public Game() {
+        window = new JFrame();
+    
         window.setSize(800,600);
         window.setTitle("Top Down Shooter");
         // tells Java that when X is clicked, end the program
@@ -11,5 +24,21 @@ public class Game {
         // centers the window on screen
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g); // clears the panel
+                g.setColor(Color.BLUE);
+                g.fillRect(playerX, playerY, 40, 40);
+            }
+
+        };
+
+        window.add(panel);
+    }
+
+    public static void main(String[] args) {
+        new Game();
     }
 }
